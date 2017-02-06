080816dd9debd8de8baf844df72df0bdb47f6b4b
Jim Heald <james.r.heald@gmail.com>
2017-02-06 14:01:11 -0800

    working on making POST and GET interact

--- http_server.py
+++ http_server.py
@@ -1,26 +1,49 @@
 #!/usr/bin/env python
  
 from http.server import BaseHTTPRequestHandler, HTTPServer
- 
+import json
+
 # HTTPRequestHandler class
-class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
- 
-  # GET
-  def do_GET(self):
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler): 
+    def __init__(self, req, addr, ser):
+        BaseHTTPRequestHandler.__init__(self, req, addr, ser)
+        print("Jim")
+    
+    def _set_headers(self):
         # Send response status code
         self.send_response(200)
- 
+
         # Send headers
         self.send_header('Content-type','text/html')
         self.end_headers()
- 
-        # Send message back to client
-        message = "You GOT me!"
-        #message = f"You are at {self.path}" 
-        # Write content as utf-8 data
-        self.wfile.write(bytes(message, "utf8"))
 
-        return
+    # GET
+    def do_GET(self):
+        self._set_headers()
+        
+        # print(self.string)
+
+        # json_data = json.dumps(self.my_data)
+        # json_bytes = str.encode(json_data)
+        # message = json_bytes 
+
+        # message = f"You are at {self.path}" 
+        # Write content as utf-8 data
+        # self.wfile.write(message)
+
+        # return
+
+    def do_POST(self):
+        self._set_headers()
+
+        length = int(self.headers['content-length'])
+        raw_data = self.rfile.read(length)
+        raw_data = str(raw_data, 'utf-8')
+        # self.my_data = json.loads(raw_data)
+        # print(self.string)
+        # self.string += "my"
+        # print(self.string)
+
     def parse_url(self):
         args = [x for x in self.path.split('/') if x != '']
  
